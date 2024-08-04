b286157137e14034a8d0d634f56ed6593a5c9c07
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-08-04 11:28:21 +0530

    add brform

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 
+
+
 export const ThemeContext = createContext();
 
 function App() {
@@ -42,6 +44,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
+            
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
---
+++ src/pages/B_R_Form.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+function B_R_Form() {
+  return <Typography variant="h2">Blood Request Form</Typography>;
+}
+
+export default B_R_Form;
