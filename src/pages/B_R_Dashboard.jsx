1c557cd1f8bcb207191e048ffe25bc22746321e6
tharushasamarawickrama <140165950+tharushasamarawickrama@users.noreply.github.com>
2024-08-04 14:05:18 +0530

    1

--- src/pages/B_R_Dashboard.jsx
+++ src/pages/B_R_Dashboard.jsx
@@ -1,10 +1,13 @@
 import { Typography } from "@mui/material";
 
 function B_R_Dashboard() {
-  return <Typography variant="h2">
-    Blood Request dashboard
+  return 
+  <div>
+    <Typography variant="h2">
+    Blood Request 
     <p>**this is your blood request dashboard**</p>
     </Typography>;
+  </div>
 }
 
 export default B_R_Dashboard;
--- src/pages/B_R_Form.jsx
+++ src/pages/B_R_Form.jsx
@@ -1,10 +1,13 @@
 import { Typography } from "@mui/material";
 
 function B_R_Form() {
-  return <Typography variant="h2">
+  return 
+  <div>
+    <Typography variant="h2">
     Blood Request Form
     <p>**what is your blood group**</p>
     </Typography>;
+  </div>
 }
 
 export default B_R_Form;
